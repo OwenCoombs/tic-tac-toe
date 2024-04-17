232436b22084a475fcbdf8f1259b2d63e3b65ecb
Owen Coombs <owencoombs03@gmail.com>
2024-04-17 16:54:15 -0400

    created almost mvp
    almost....

---
+++ js/main.js
@@ -0,0 +1,111 @@
+
+var currentPlayer = 'X'; // Start with player X
+
+  var box1 = document.getElementById('b1');
+  var box2 = document.getElementById('b2');
+  var box3 = document.getElementById('b3');
+  var box4 = document.getElementById('b4');
+  var box5 = document.getElementById('b5');
+  var box6 = document.getElementById('b6');
+  var box7 = document.getElementById('b7');
+  var box8 = document.getElementById('b8');
+  var box9 = document.getElementById('b9');
+
+  box1.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box2.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box3.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box4.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box5.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box6.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box7.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box8.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
+
+  box9.addEventListener('click', function() {
+    this.querySelector('.box-content').textContent = currentPlayer;
+    for (var i = 0; i < 1; i++) {
+    if (currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else {
+        currentPlayer = 'X';
+    }
+}
+  });
