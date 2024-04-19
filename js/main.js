ca307d60249b8bb82982e020d1899ef9a776a24c
Owen Coombs <owencoombs03@gmail.com>
2024-04-19 09:56:25 -0400

    done
    done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 
 let currentPlayer = 'X'; // Start with player X
 let isInWinState = false;
+let btnReset = document.getElementById('btnReset');
+
+// event click listner for button
+function init(){
+btnReset.addEventListener('click',reloadGame)
+}
+// function for reloading game
+function reloadGame(){
+    location.reload();
+    boxes = '';
+    
+}
 
 
 
 
 
 
+// creating vars for all boxes
 let box1 = document.getElementById('b1');
 let box2 = document.getElementById('b2');
 let box3 = document.getElementById('b3');
@@ -17,7 +30,8 @@ let box7 = document.getElementById('b7');
 let box8 = document.getElementById('b8');
 let box9 = document.getElementById('b9');
 let boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
-
+// adding all boxes into one var
+//winning combos
 const winningCombos = [
     [box1, box2, box3],
     [box4, box5, box6],
@@ -28,37 +42,36 @@ const winningCombos = [
     [box1, box5, box9],
     [box3, box5, box7]
 ];
-
+// creating function to make boxes clickable
 boxes.forEach(box => {
     box.addEventListener('click', function(event) {
         if(isInWinState){
             return;
-
+            
         }
-        // console.log(event.target);
-        // console.log(this);
-        let boxContent = this.querySelector('.box-content');
-        // console.log(typeof boxContent);
-        // console.log( this.querySelector('.box-content').textContent);
-        // this.querySelector('.box-content').textContent = currentPlayer;
-        // console.log( this.querySelector('.box-content').textContent);
+     
+        let boxContent = this.querySelector('.box-content'); // this in code, it's talking about the object that's currently "doing something."
+     // if there is an empty box it will display current player
         if (boxContent.textContent === ''){
             boxContent.textContent = currentPlayer
             
         }
-
+        // checking who won and displaying winner on screen
         if(checkWin(currentPlayer)){
-            document.getElementById('winner').textContent = "Winner is " + currentPlayer
+            document.getElementById('winner').textContent = "Winner is: " + currentPlayer
             return;
         }
-
+        
         // for (let i = 0; i < 1; i++) {
-        if (currentPlayer === 'X') {
+
+        // player tracker
+            if (currentPlayer === 'X') {
             currentPlayer = 'O';
             
         } else {
             currentPlayer = 'X';
         }
+        // displaying current player onto screen
         document.getElementById('trackPlayer').textContent = "Current Player:  " + currentPlayer
         checkTie()
         // }
@@ -82,12 +95,14 @@ function checkWin(currentPlayer){
             // winningCombos[i].forEach(box => console.log(box.querySelector(".box-content").textContent))
             
         if (winningCombos[i].every(value => value.querySelector(".box-content").textContent === currentPlayer)){
-            
+           
             console.log("winner ", currentPlayer);
             isInWinState = true
             return true
+            
         }
     }
+    // displaying current winner
     document.getElementById('winner').textContent === 'Winner is: '
 }
 
@@ -108,3 +123,4 @@ function checkTie() {
     return true;
 }
 
+init()
